b8e6a4fee0f61e5dcbfc831a8e228d1de16aaadd
Deepak2mca <sahu.deepak01@gmail.com>
2018-03-14 03:59:03 +0530

    files added in repository

---
+++ EmployeeCRUD.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+   public class EmployeeCRUD
+    {
+        Dictionary<int, Employee> employeeList = new Dictionary<int, Employee>();
+        public  EmployeeCRUD()
+        {
+            employeeList.Add(1, new Employee() { EmployeeID = 1, EmployeeName = "Tom miller",ManagerId=1, TopLowerEmployeeIDs = new HashSet<int> { } });
+            employeeList.Add(2, new Employee() { EmployeeID = 2, EmployeeName = "Jason Brazier",ManagerId=0, TopLowerEmployeeIDs = new HashSet<int> { 1} });
+        }
+ 
+        public void AddUpdateEmployee(Employee emp)
+        {
+            if (!employeeList.ContainsKey(emp.EmployeeID))
+                employeeList.Add(emp.EmployeeID, emp); // add new entry
+            else
+                employeeList[emp.EmployeeID] = emp; // update entry value
+        }
+        public Employee GetEmployeeByEMPId(int empid)
+        {
+            return employeeList.FirstOrDefault(emp => emp.Key == empid).Value; 
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> lstOfEmployee = new List<Employee>();
+            Dictionary<int, Employee>.ValueCollection values = employeeList.Values;
+            foreach (Employee emp in values)
+            {
+                lstOfEmployee.Add(emp);
+            }
+            return lstOfEmployee;
+        }
+
+        public bool DeleteEmployeeByEmployeeId(int empid)
+        {
+          return employeeList.Remove(empid);
+        }
+       /// <summary>
+       /// Remove employee reporting to a manager
+       /// </summary>
+       /// <param name="managerid"></param>
+        public void DeleteAllEmployeeByManagerId(int managerid)
+        {
+            Employee manager = employeeList.FirstOrDefault(emp => emp.Key == managerid).Value;  
+            foreach(int empid in manager.TopLowerEmployeeIDs)
+            {
+                DeleteEmployeeByEmployeeId(empid);
+            }
+        }
+        /// <summary>
+        /// Remove manager from 
+        /// </summary>
+        /// <param name="managerid"></param>
+        /// <param name="employeeID"></param>
+        public void ChangingReportingManager(int managerid, int employeeid, int newManagerID)
+        {
+            //remove employee from current manager 
+            Employee oldmanager = employeeList.FirstOrDefault(emp => emp.Key == managerid).Value;
+            oldmanager.TopLowerEmployeeIDs.Remove(employeeid);
+           
+            //assign employee to another manage
+            Employee newmanager = employeeList.FirstOrDefault(emp => emp.Key == newManagerID).Value;
+            newmanager.TopLowerEmployeeIDs.Add(employeeid);
+
+            //update employee managerid 
+            Employee employee = employeeList.FirstOrDefault(emp => emp.Key == employeeid).Value;
+            employee.ManagerId = 0;            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="managerid"></param>
+        HashSet<int> allEmployeeIds=new HashSet<int>();
+
+
+
+        public List<Employee> GetEmployeeListByManagerId(int managerid)
+        {
+            GetAllEmployeeIDs(managerid);          
+            var lstEmployee = employeeList.Keys.Where(allEmployeeIds.Contains).Select(x => employeeList[x]).ToList();
+            return lstEmployee;
+        }
+        private void GetAllEmployeeIDs(int managerid)
+        {
+            
+            var managerdetails = GetEmployeeByEMPId(managerid);
+            allEmployeeIds.UnionWith(managerdetails.TopLowerEmployeeIDs);  
+            foreach(int empid in managerdetails.TopLowerEmployeeIDs)
+            {
+                GetAllEmployeeIDs(empid);
+            }
+        }
+
+
+        //foreach (int empid in manager.TopLowerEmployeeIDs)
+        //    {
+        //        if (empid == employeeid)
+        //        {
+
+        //            //Employee tobeUpdatedEmployee=GetEmployeeByEMPId(empid);
+        //            //tobeUpdatedEmployee.ManagerId = 0;
+        //            //employeeList[empid] = tobeUpdatedEmployee;
+        //        }
+        //    }
+
+
+        
+    }
+}
---
+++ EmployeeTest.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Employee.Models;
+
+namespace Employee.Tests
+{
+    [TestClass]
+   public class EmployeeTest
+    {
+        
+        /// <summary>
+        /// Add and update employeeList
+        /// </summary>
+        [TestMethod]
+        public void AddUpdateEmployee()
+        {
+            // arrange  
+            // arrange  
+            EmployeeCRUD objEmplyeeCRUD = new EmployeeCRUD();
+            var emp = new Employee.Models.Employee()
+            {
+                EmployeeID = 3,
+                EmployeeName = "Tom miller",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> { }
+            };
+
+            // act  
+            objEmplyeeCRUD.AddUpdateEmployee(emp);
+            List<Employee.Models.Employee> lstEmployees= objEmplyeeCRUD.GetEmployees();
+            var savedEmp=  lstEmployees.Find(x => x.EmployeeID == 3);
+
+
+            // assert  
+            
+            Assert.AreEqual(savedEmp.EmployeeName, emp.EmployeeName);  
+
+                        
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void RemoveAllEmployeeReportingToManager()
+        {
+            // arrange  
+            EmployeeCRUD objEmplyeeCRUD = new EmployeeCRUD();
+            var emp1 = new Employee.Models.Employee()
+            {
+                EmployeeID = 3,
+                EmployeeName = "Tom miller3",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> { }
+            };
+            var emp2 = new Employee.Models.Employee()
+            {
+                EmployeeID = 4,
+                EmployeeName = "Tom miller4",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> {3,5 }
+            };
+
+            var emp3 = new Employee.Models.Employee()
+            {
+                EmployeeID = 5,
+                EmployeeName = "Tom miller5",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> { }
+            };
+
+
+            // act 
+            //adding above employee object into collection
+            objEmplyeeCRUD.AddUpdateEmployee(emp1);
+            objEmplyeeCRUD.AddUpdateEmployee(emp2);
+            objEmplyeeCRUD.AddUpdateEmployee(emp3);
+
+            // deleting all employee who has manager "Tom miller4"
+            // employee 3 and 5 will removed after execution of function.
+            objEmplyeeCRUD.DeleteAllEmployeeByManagerId(4);
+
+            //assert
+
+
+
+
+            
+        }
+        [TestMethod]
+        public void ChaingingReportingOfAnEmployee()
+        {
+            EmployeeCRUD objEmplyeeCRUD = new EmployeeCRUD();
+            var emp1 = new Employee.Models.Employee()
+            {
+                EmployeeID = 3,
+                EmployeeName = "Tom miller3",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> { }
+            };
+            //Employee ID 3 is reporting to emp 4 
+            var emp2 = new Employee.Models.Employee()
+            {
+                EmployeeID = 4,
+                EmployeeName = "Tom miller4",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> { 3 }
+            };
+           
+            var emp3 = new Employee.Models.Employee()
+            {
+                EmployeeID = 5,
+                EmployeeName = "Tom miller5",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> {  }
+            };
+
+            // act 
+            //adding above employee object into collection
+            objEmplyeeCRUD.AddUpdateEmployee(emp1);
+            objEmplyeeCRUD.AddUpdateEmployee(emp2);
+            objEmplyeeCRUD.AddUpdateEmployee(emp3);
+
+            // currently employee "Tom miller3" is reporting to "Tom miller4"
+            // now I am changing employee "Tom miller3" is reporting to "Tom miller5"
+            objEmplyeeCRUD.ChangingReportingManager(4, 3, 5);
+
+
+
+
+        }
+
+        [TestMethod]
+
+        public void GetAllEmployeeofAnManager()
+        {
+            EmployeeCRUD objEmplyeeCRUD = new EmployeeCRUD();
+            var emp1 = new Employee.Models.Employee()
+            {
+                EmployeeID = 3,
+                EmployeeName = "Tom miller3",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> {1,2 }
+            };
+            //Employee ID 3 is reporting to emp 4 
+            var emp2 = new Employee.Models.Employee()
+            {
+                EmployeeID = 4,
+                EmployeeName = "Tom miller4",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> { 3 }
+            };
+           
+            var emp3 = new Employee.Models.Employee()
+            {
+                EmployeeID = 5,
+                EmployeeName = "Tom miller5",
+                ManagerId = 0,
+                TopLowerEmployeeIDs = new HashSet<int> { 4 }
+            };
+
+            // act 
+            //adding above employee object into collection
+            objEmplyeeCRUD.AddUpdateEmployee(emp1);
+            objEmplyeeCRUD.AddUpdateEmployee(emp2);
+            objEmplyeeCRUD.AddUpdateEmployee(emp3);
+
+            
+            List<Employee.Models.Employee> lst = objEmplyeeCRUD.GetEmployeeListByManagerId(5);
+
+           
+            
+        }
+    }
+}
